4ddddc49c6d3e6e1f418ad752881214b198c8ef3
Filipemt <contato.motadev@gmail.com>
2026-07-20 19:22:59 -0300

    raw(pedidos): feign client settings

---
+++ pedidos/src/main/java/com/filipecode/icompras/pedidos/config/ClientsConfig.java
@@ -0,0 +1,11 @@
+package com.filipecode.icompras.pedidos.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.filipecode.icompras.pedidos.client")
+public class ClientsConfig {
+
+    
+}
