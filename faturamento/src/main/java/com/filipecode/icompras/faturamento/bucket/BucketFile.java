ede33fc1fe3e2388c044c4979eaacefec2b60658
Filipemt <contato.motadev@gmail.com>
2026-08-01 00:05:37 -0300

    docs(faturamento): add logo to nota fiscal PDF

--- faturamento/src/main/java/com/filipecode/icompras/faturamento/bucket/BucketFile.java
+++ faturamento/src/main/java/com/filipecode/icompras/faturamento/bucket/BucketFile.java
@@ -2,7 +2,6 @@ package com.filipecode.icompras.faturamento.bucket;
 
 import org.springframework.http.MediaType;
 
-import java.awt.*;
 import java.io.InputStream;
 
 public record BucketFile(String name, InputStream inputStream, MediaType mediaType, long size) {
