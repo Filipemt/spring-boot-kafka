4be01ad9708572b4da962807b7e4c8e4b7e35ea1
Filipemt <contato.motadev@gmail.com>
2026-08-10 23:01:31 -0300

    feat(clientes): add flyway migrations for clientes table and seed data

--- icompras-servicos/database/schema.sql
+++ icompras-servicos/database/schema.sql
@@ -7,16 +7,6 @@ create table produtos (
 
 
 create database icomprasclientes;
-create table clientes (
-                          codigo serial not null primary key,
-                          nome varchar(150) not null,
-                          cpf char(11) not null,
-                          logradouro varchar(100),
-                          numero varchar(10),
-                          bairro varchar(100),
-                          email varchar(150),
-                          telefone varchar(20)
-);
 
 create database icompraspedidos;
 create table pedidos (
