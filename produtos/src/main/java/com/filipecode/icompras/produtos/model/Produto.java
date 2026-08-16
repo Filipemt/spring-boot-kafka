29f2dc581d85e60b3ab449da3c97a565c844b615
Filipemt <contato.motadev@gmail.com>
2026-08-16 19:15:52 -0300

    feat(produtos): model produto with active flag and domain methods

--- produtos/src/main/java/com/filipecode/icompras/produtos/model/Produto.java
+++ produtos/src/main/java/com/filipecode/icompras/produtos/model/Produto.java
@@ -12,11 +12,33 @@ public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
     private Long codigo;
 
     @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
-    @Column(name = "valor_unitario",  nullable = false, precision = 16, scale = 2)
+    @Column(name = "valor_unitario", nullable = false, precision = 16, scale = 2)
     private BigDecimal valorUnitario;
+
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo = true;
+
+    public void atualizarDados(String nome, BigDecimal valorUnitario) {
+        this.nome = nome;
+        this.valorUnitario = valorUnitario;
+    }
+
+    public void atualizarDadosParciais(String nome, BigDecimal valorUnitario) {
+        if (nome != null) {
+            this.nome = nome;
+        }
+        if (valorUnitario != null) {
+            this.valorUnitario = valorUnitario;
+        }
+    }
+
+    public void desativar() {
+        this.ativo = false;
+    }
 }
